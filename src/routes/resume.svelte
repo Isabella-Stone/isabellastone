aba889a6f5f54ff54a33cc16b29b3e156a223328
Isabella Stone <istone@stevens.edu>
2022-08-07 14:07:34 -0400

    add resume pdf

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -1,15 +1,5 @@
-<style>
-    img {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        /* width: 50%; */
-        /* height: 100%; */
-        max-width: 100%;
-        /* height: 130vh; */
-        /*width: 100vh; */
-        object-fit: cover;
-    }
-</style>
-
-<img src="/resume.jpg" alt="Resume">
+<body>
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <iframe src="/resume.pdf" width="100%" height="1000px">
+    </iframe>
+</body>
