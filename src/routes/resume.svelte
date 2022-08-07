2eeabda171594ce91db23bc07e8a4d870a0030b6
Isabella Stone <istone@stevens.edu>
2022-08-07 17:56:12 -0400

    fix resume pdf

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -1,5 +1,5 @@
 <body>
     <!-- svelte-ignore a11y-missing-attribute -->
-    <iframe src="/resume.pdf#view=fitH" width="100%" height="800px">
+    <iframe src="/resume.pdf#view=fitH" width="100%" height="750px">
     </iframe>
 </body>
