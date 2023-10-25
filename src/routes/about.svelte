de63c591e3db0cc341b1d2e30d19479176810154
Isabella Stone <52970853+Isabella-Stone@users.noreply.github.com>
2023-10-25 11:59:29 -0400

    update about me

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -33,7 +33,7 @@
     <ul class = "facts">
         <li>I am from <span style="color:#e7621a">Staten Island, NY</span></li>
         <li>My favorite pastimes are listening to <span style="color:#125572">music</span> and <span style="color:#f18190">baking</span></li>
-        <li>My current favorite coding language is <span style="color:#b7800a">Java</span></li>
-        <li>My favorite class I have taken so far is <span style="color:#5f8d88">Algorithms</span></li>
+        <li>My current favorite coding language is <span style="color:#b7800a">JavaScript</span></li>
+        <li>My favorite class I have taken so far is <span style="color:#5f8d88">Web Programming</span></li>
     </ul>
 </div>
